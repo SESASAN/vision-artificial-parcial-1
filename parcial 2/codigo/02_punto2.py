fa3507dc3e1a0091cb8df62238e8d9e9432b8190
SESASAN <sebastianjesus.perez@gmail.com>
2026-09-25 17:05:35 -0500

    feat: resolver el punto 2 del parcial 2 (especificación de histograma)
    Usa skimage.exposure.match_histograms con Referencia.tif como imagen
    objetivo.

--- parcial 2/codigo/02_punto2.py
+++ parcial 2/codigo/02_punto2.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 import cv2
+from skimage.exposure import match_histograms
 
 import sys
 sys.path.append("..")
@@ -10,10 +11,27 @@ ref = np.array(Image.open("../images/Referencia.tif").convert("L"))
 plt.imshow(ref, cmap="gray")
 plt.axis("off")
 
-# TODO: calcular y graficar el histograma de ref
+hist_ref = cv2.calcHist([ref], [0], None, [256], [0, 256]).flatten()
 
-# TODO: cargar gray (imagen original en escala de grises, ver Punto 1)
-# TODO: aplicar especificación de histograma usando ref como objetivo
-gray_spec = None
+plt.figure()
+plt.plot(hist_ref)
+plt.xlabel("Nivel de intensidad")
+plt.ylabel("Numero de pixeles")
+plt.savefig("../results/punto2_histograma_referencia.png", bbox_inches="tight")
 
-# TODO: calcular y graficar el histograma de gray_spec
+img = np.array(Image.open("../images/mi_imagen.png").convert("RGB"))
+gray = np.array(Image.fromarray(img).convert("L"))
+
+gray_spec = match_histograms(gray, ref).astype(np.uint8)
+
+plt.imshow(gray_spec, cmap="gray")
+plt.axis("off")
+plt.savefig("../results/punto2_especificacion.png", bbox_inches="tight", pad_inches=0)
+
+hist_spec = cv2.calcHist([gray_spec], [0], None, [256], [0, 256]).flatten()
+
+plt.figure()
+plt.plot(hist_spec)
+plt.xlabel("Nivel de intensidad")
+plt.ylabel("Numero de pixeles")
+plt.savefig("../results/punto2_histograma_especificacion.png", bbox_inches="tight")
